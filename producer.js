cb2f0f5fca17502a20fc21e1ca68c9250bbf594f
Alessio Coser <alessio.coser@xpeppers.com>
2017-11-24 09:47:18 +0100

    produce a given message, and echoes script usage if arguments aren't enough

--- producer.js
+++ producer.js
@@ -1,4 +1,15 @@
+const path = require('path')
 const amqp = require('amqplib')
+const args = process.argv
+var message = null
+
+if (args.length !== 3) {
+  console.log('Usage:\n')
+  console.log(path.basename(args[0]) + ' ' + path.basename(args[1]) + ' "[message]"')
+  process.exit(1)
+}
+
+message = args[2]
 
 async function producer (queueName, connectionString) {
   var connection = await amqp.connect(connectionString)
@@ -6,7 +17,7 @@ async function producer (queueName, connectionString) {
   var ok = await channel.assertQueue(queueName)
 
   if (ok) {
-    await channel.sendToQueue(queueName, Buffer.from('something to do'))
+    await channel.sendToQueue(queueName, Buffer.from(message))
   }
 
   await channel.close()
